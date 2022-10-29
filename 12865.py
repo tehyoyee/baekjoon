b7434b334303abd8b7ad4b087d187e2f0f2afaa4
root <root@tehyoyee.localdomain>
2022-10-30 02:07:45 +0900

    add 12865.py

--- 12865.py
+++ 12865.py
@@ -1,23 +1,22 @@
 # 12865.py
 
+# 다시풀기
+
 import sys
-import copy
 input = sys.stdin.readline
 
-def search(goods, ref, dp):
-	for i in range(len(goods)):
-		if ref + goods[i][0] <= k:
-			temp = copy.deepcopy(goods)
-			temp.pop(i)
-			dp[ref + goods[i][0]] = max(dp[ref] + goods[i][1], dp[ref + goods[i][0]])
-			search(temp, ref + goods[i][0], dp)
-
 n, k = map(int, input().split())
-goods = []
-dp = [0] * (k + 1)
+goods = [[0, 0]]
+dp = [[0] * (k + 1) for _ in range(n + 1)]
 for _ in range(n):
 	a, b = map(int, input().split())
 	goods.append([a, b])
-
-search(copy.deepcopy(goods), 0, dp)
-print(max(dp))
+for i in range(1, n + 1):
+	for j in range(1, k + 1):
+		w = goods[i][0]
+		v = goods[i][1]
+		if w > j:
+			dp[i][j] = dp[i - 1][j]
+		else:
+			dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+print(dp[n][k])
