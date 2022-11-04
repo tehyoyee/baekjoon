9a06ef3d2c1fc436c2f55cdb2c8add10d9f6c692
taehykim <tehyoyee@gmail.com>
2022-11-04 19:48:41 +0900

    add 1946.py

--- 1946.py
+++ 1946.py
@@ -6,17 +6,15 @@ input = sys.stdin.readline
 t = int(input())
 
 for _ in range(t):
-	cut = 0
 	n = int(input())
-	check = [1] * (n)
+	result = 1
 	case = []
 	for _ in range(n):
 		case.append(list(map(int, input().split())))
 	case.sort()
-	print(case)
-	for i in range(n - 1):
-		cut = max(case[i][1], cut)
-		for j in range(i + 1, n):
-			if case[j][1] > cut:
-				check[j] = 0
-print(check)
+	cut = case[0][1]
+	for i in range(1, n):
+		if case[i][1] < cut:
+			result += 1
+			cut = min(cut, case[i][1])
+print(result)
