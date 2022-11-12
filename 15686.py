dd4efec1f7d2f7464ce0ec68a583bd0fa518acbe
taehykim <tehyoyee@gmail.com>
2022-11-13 01:33:20 +0900

    add 15686.py

--- 15686.py
+++ 15686.py
@@ -3,43 +3,35 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
+from itertools import combinations
 
 n, m = map(int, input().split())
 graph = []
-result = 0
+chickens = []
 houses = []
-chickens = []	
-candi = []
+
+def bfs(i, j, steps):
+	q = [[i, j]]
+	while q:
+		i_cur, j_cur = q.pop(0)
+		
 
 for i in range(n):
 	graph.append(list(map(int, input().split())))
 	for j in range(n):
-		if graph[i][j] == 1:
-			houses.append([i, j, 10000, 100000])
-		elif graph[i][j] == 2:
+		if graph[i][j] == 2:
 			chickens.append([i, j])
-
-for house in houses:
-	for i in range(len(chickens)):
-		if house[3] > abs(house[0] - chickens[i][0]) + abs(house[1] - chickens[i][1]):
-			house[3] = abs(house[0] - chickens[i][0]) + abs(house[1] - chickens[i][1])
-			house[2] = i
-
-def find(chicken_list, idx, m):
-	if m == 0:
-		return(chicken_list)
-	for i in range(idx + 1, len(chickens)):
-		find(chicken_list + [i], i, m - 1)
-
-candi.append(find([], -1, m))
-result = [[0] * n]
-for house in houses:
-	dist = 11110
-	for i in candi:
-		tmp = 0
-		for j in len(candi[i]):
-			tmp += abs(candi[i][j] - )
-
-print(graph)
+		elif graph[i][j] == 1:
+			houses.append([i, j])
 print(chickens)
-print(houses)
+coms = combinations(chickens, m)
+result = 0
+for com in coms:
+	dis_com = 1000000
+	for house in houses:
+		dis = 1000000
+		for chicken in chickens:
+			dis = min(dis, abs(house[0] - chicken[0]) + abs(house[1] - chicken[1]))
+		dis_com += dis
+	result += dis_com
+print(list(arr))
