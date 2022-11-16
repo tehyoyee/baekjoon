2a32ffff860442c289c2103664ea5b8235b0abcc
taehykim <tehyoyee@gmail.com>
2022-11-16 19:55:24 +0900

    add 2630.py

--- 2630.py
+++ 2630.py
@@ -8,31 +8,37 @@ graph = [[0] * (n + 1)]
 for _ in range(n):
 	graph.append([0] + list(map(int, input().split())))
 visit = [[0] * (n + 1) for _ in range(n + 1)]
+global white 
 white = 0
+global blue
 blue = 0
 
-def s(i, j, l, color):
-	if i + 1 > n or j + 1 > n:
-		return
-	graph[i][j] = 2
-	for k in range(l + 1):
-		if not 1 <= i + 1 - l <= n or not 1 <= j + 1 - l <= n:
-			return
-		if graph[i + 1 - k][j + 1] != color or graph[i + 1][j + 1 - k] != color:
-			return
-	for k in range(l + 1):
-		graph[i + 1 - k][j + 1] = 2
-		graph[i + 1][j + 1 - k] = 2
-	s(i + 1, j + 1, l + 1, color)
+def check(i, j, l, color):
+	for m in range(i, i + l):
+		for n in range(j, j + l):
+			if graph[m][n] != color:
+				return False
+	return True
 
-for i in range(1, n + 1):
-	for j in range(1, n + 1):
-		if graph[i][j] != 2:
-			if graph[i][j] == 0:
-				white += 1
-			else:
-				blue += 1
-			s(i, j, 1, graph[i][j])
+def s(i, j, l):
+	global white
+	global blue
+	if check(i, j, l, graph[i][j]):
+		if graph[i][j] == 0:
+			white += 1
+			return
+		else:
+			blue += 1
+			return
+	else:
+		l //= 2
+		if l == 0:
+			return
+		s(i, j, l)
+		s(i, j + l, l)
+		s(i + l, j, l)
+		s(i + l, j + l, l)
+s(1, 1, n)
 
 print(white)
 print(blue)
