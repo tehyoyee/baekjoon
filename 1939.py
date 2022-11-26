dc431b20763fbb7b7a41f12ef0c03d528bd085fe
root <root@tehyoyee.localdomain>
2022-11-26 21:22:54 +0900

    add 1939.py

---
+++ 1939.py
@@ -0,0 +1,15 @@
+# 1939.py
+
+import sys
+input = sys.stdin.readline
+
+def union_parent(a, b):
+	a = find_parent
+def find_parent(x):
+	if graph[x] = x:
+		return x
+	graph[x][0] = find_parent(graph[x][0])
+n, m = map(int, input().split())
+graph = [[x, 0] for x in range(n + 1)]
+# for i in range(m):
+print(graph)
