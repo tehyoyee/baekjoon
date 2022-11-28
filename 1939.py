a433f401a8baeaca111c39c6d3ba39023517f53a
root <root@tehyoyee.localdomain>
2022-11-29 07:08:03 +0900

    add 1939.py

--- 1939.py
+++ 1939.py
@@ -3,13 +3,30 @@
 import sys
 input = sys.stdin.readline
 
-def union_parent(a, b):
-	a = find_parent
 def find_parent(x):
-	if graph[x] = x:
+	if parent[x] == x:
 		return x
-	graph[x][0] = find_parent(graph[x][0])
+	else:
+		parent[x] = find_parent(parent[x])
+	return parent[x]
+
+def union_parent(a, b):
+	a = find_parent(a)
+	b = find_parent(b)
+	parent[max(a, b)] = min(a, b)
+
 n, m = map(int, input().split())
-graph = [[x, 0] for x in range(n + 1)]
-# for i in range(m):
-print(graph)
+parent = [x for x in range(n + 1)]
+graph = []
+for i in range(m):
+	a, b, c = map(int, input().split())
+	graph.append([c, a, b])
+m, n = map(int, input().split())
+graph.sort(reverse=True)
+for x in graph:
+	union_parent(x[1], x[2])
+	if find_parent(n) == find_parent(m):
+		print(x[0])
+		break
+# print(graph)
+# print(parent)
