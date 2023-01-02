d5e5b96252895d240adc308bc019bc58e9f6814a
taehykim <tehyoyee@gmail.com>
2023-01-02 21:32:45 +0900

    add 1715.py

--- 1715.py
+++ 1715.py
@@ -1,34 +1,50 @@
 # 1715.py
 
+# import sys
+# input = sys.stdin.readline
+# from collections import deque
+
+# n = int(input())
+# arr = []
+# for _ in range(n):
+# 	arr.append(int(input()))
+# arr.sort()
+# q = deque(arr)
+# result = 0
+
+# while n >= 2:
+# 	i = 0
+# 	a = q.popleft()
+# 	b = q.popleft()
+# 	result += (a + b)
+# 	a += b
+# 	for _ in range(n - 2):
+# 		if a < q[0]:
+# 			break
+# 		i += 1
+# 		q.rotate(-1)
+# 	q.appendleft(a)
+# 	q.rotate(i)
+# 	n -= 1
+# 	print(q)
+
+# print(result)
+
 import sys
 input = sys.stdin.readline
+from heapq import heappush, heappop
+
+n = int(input())
+heap = []
+for _ in range(n):
+	heappush(heap, int(input()))
 
 result = 0
-n = int(input())
-lst = []
-for _ in range(n):
-	lst.append(int(input()))
-lst.sort()
-
-while n >= 3:
-	if lst[0] > lst[-2] and lst[0] > lst[-1]:
-		result += lst[-1] + lst[-2]
-		lst.append(lst.pop(-1) + lst.pop(-1))
-	elif lst[1] > lst[-1] and lst[1] > lst[0]:
-		result += lst[0] + lst[-1]
-		lst.append(lst.pop(0) + lst.pop(-1))
-	elif lst[-1] > lst[0] and lst[-1] > lst[1]:
-		result += lst[0] + lst[1]
-		lst.append(lst.pop(0) + lst.pop(0))
+while n >= 2:
+	a = heappop(heap)
+	a += heappop(heap)
+	result += a
+	heappush(heap, a)
 	n -= 1
-result += sum(lst)
+
 print(result)
-
-
-1 2 3 4 100 101 102 103
-3 4 100 101 102 103 3
-100 101 102 103 3 7
-100 101 102 103 10
-101 102 103 110
-103 110 103
-110 206
