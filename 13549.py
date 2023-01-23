02b59ceec590002c80e5532da91b43d24b3d69ae
root <root@tehyoyee.localdomain>
2023-01-23 16:46:03 +0900

    Add 2343.py

--- 13549.py
+++ 13549.py
@@ -11,18 +11,17 @@ dp = [j for j in range(n, -1, -1)] + [i for i in range(1, 2 * r + 1)]
 if n >= k:
 	print(n - k)
 else:
-	for i in range(n, r):
+	for i in range(n, 2* r):
 		j = 1
 		while i * j < r + r:
 			dp[i * j] = min(dp[i * j], dp[i])
 			j *= 2
-	for i in range(n, r):
+	for i in range(n, 2 * r):
 		dp[i] = min(dp[i - 1] + 1, dp[i], dp[i + 1] + 1)
 		j = 1
 		while i * j < r + r:
 			dp[i * j] = min(dp[i * j], dp[i])
 			j *= 2
-	print(dp)
 	for i in range(1, r + r):
 		dp[i] = min(dp[i - 1] + 1, dp[i], dp[i + 1] + 1)
 	print(dp[k])
