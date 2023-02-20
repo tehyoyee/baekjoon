01be5c82568e0e6bfc846a593eeeddfeb0ec6acc
taehykim <tehyoyee@gmail.com>
2023-02-20 15:27:17 +0900

    Add 6588.py

--- 6588.py
+++ 6588.py
@@ -3,33 +3,30 @@
 
 import sys
 input = sys.stdin.readline
+print = sys.stdout.write
 import math
 
 dp = [False] * 1000000
 cur_range = 3
-def search(last_range, cur_range):
-	for i in range(last_range, cur_range):
-		flag = 0
-		for j in range(2, int(math.sqrt(i)) + 1):
-			if i % j == 0:
-				flag = 1
-				break
-		if flag == 0:
-			dp[i] = True
+
+for i in range(3, 1000000, 2):
+	flag = 0
+	for j in range(2, int(math.sqrt(i)) + 1):
+		if i % j == 0:
+			flag = 1
+			break
+	if flag == 0:
+		dp[i] = True
 
 while True:
 	n = int(input())
 	if n == 0:
 		break
-	if cur_range < n:
-		last_range = cur_range
-		cur_range = n
-		search(last_range, cur_range)
 	flag = 0
-	for i in range(3, n // 2 + 1):
+	for i in range(3, n // 2 + 1, 2):
 		if dp[i] == True and dp[n - i] == True:
-			print(n, "=", i, "+", n - i)
+			print(f"{n} = {i} + {n - i}\n")
 			flag = 1
 			break
 	if not flag:
-		print("Goldbach's conjecture is wrong.")
+		print("Goldbach's conjecture is wrong.\n")
