9f9b4740c8516d37d472f7de2b86b9c6a56e98f7
taehykim <tehyoyee@gmail.com>
2023-02-21 11:13:50 +0900

    add 1325.py

--- 1325.py
+++ 1325.py
@@ -1,27 +1,25 @@
 # 1325.py
-# 23.02.20. 16:57 ~ 19, 22~
+# 23.02.20. 16:57 ~ 19, 22~ 윽.시간초과 default
 
 import sys
 input = sys.stdin.readline
 from collections import deque
 
-result = [0, []]
-
+result = []
+max_cnt = 0
 def bfs(x):
+	cnt = 0
 	visit = [False] * (n + 1)
 	visit[x] = 1
-	q = deque([[x, 1]])
+	q = deque([x])
 	while q:
-		c, l = q.popleft()
-		for y in graph[c]:
-			if not visit[y]:
-				visit[y] = True
-				q.append([y, l + 1])
-	if l > result[0]:
-		result[0] = l
-		result[1] = [x]
-	elif l == result[0]:
-		result[1].append(x)
+		c = q.popleft()
+		cnt += 1
+		for nc in graph[c]:
+			if not visit[nc]:
+				visit[nc] = True
+				q.append(nc)
+	return cnt
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
@@ -29,6 +27,11 @@ for _ in range(m):
     b, a = map(int, input().split())
     graph[a].append(b)
 for i in range(1, n + 1):
-	bfs(i)
-for x in result[1]:
+	cnt = bfs(i)
+	if cnt > max_cnt:
+		max_cnt = cnt
+		result = [i]
+	elif cnt == max_cnt:
+		result.append(i)
+for x in result:
 	print(x, end=" ")
