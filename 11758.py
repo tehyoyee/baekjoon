207783267c57bf5be0c9e77599ade0e377962dc1
taehykim <tehyoyee@gmail.com>
2023-02-28 13:28:13 +0900

    add 11758.py

--- 11758.py
+++ 11758.py
@@ -1,28 +1,24 @@
 # 11758.py
-# 23.02.27. 18:42 ~
+# 23.02.28. 11:26 ~ 11:40, 13:16 ~
 
 pos = []
-dist = []
 for _ in range(3):
 	pos.append(list(map(float, input().split())))
-for i in range(-1, 2):
-	dist.append(pow(pos[i][0] - pos[i + 1][0], 2) + pow(pos[i][1] - pos[i + 1][1], 2))
-dist.sort()
-if dist[0] + dist[1] < dist[2]:
-	flag = -1
-else:
+if pos[1][0] - pos[0][0] >= 0:
 	flag = 1
+else:
+	flag = -1
 if pos[1][0] - pos[0][0] == 0:
 	m1 = float('inf') * (pos[1][1] - pos[0][1])
 else:
-	m1 = (pos[1][1] - pos[0][1]) / (pos[1][0] - pos[0][0])
-if pos[2][0] - pos[0][0] == 0:
-	m2 = float('inf') * (pos[2][1] - pos[0][1])
+	m1 = float((pos[1][1] - pos[0][1]) / (pos[1][0] - pos[0][0]))
+if pos[2][0] - pos[1][0] == 0:
+	m2 = float('inf') * (pos[2][1] - pos[1][1])
 else:
-	m2 = (pos[2][1] - pos[0][1]) / (pos[2][0] - pos[0][0])
-if m1 == m2:
+	m2 = float((pos[2][1] - pos[1][1]) / (pos[2][0] - pos[1][0]))
+if (pos[1][1] - pos[0][1]) * (pos[2][0] - pos[0][0]) == (pos[1][0] - pos[0][0]) * (pos[2][1] - pos[0][1]):
 	print(0)
-elif m1 > m2:
-	print(-1 * flag)
+elif m1 * (pos[2][0] - pos[0][0]) + pos[0][1]  < pos[2][1]:
+	print(flag * 1)
 else:
-	print(1 * flag)
+	print(flag * -1)
