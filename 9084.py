e6e74ae905083b24506a7e91c387b0ac2f60fc25
root <root@tehyoyee.localdomain>
2023-03-02 16:12:01 +0900

    Add 9084.py

--- 9084.py
+++ 9084.py
@@ -1,27 +1,42 @@
 # 9084.py
-# 23.03.01. 21:11 ~
+# 23.03.01. 90분
+
+# import sys
+# input = sys.stdin.readline
+
+# t = int(input())
+# for _ in range(t):
+# 	n = int(input())
+# 	tokens = list(map(int, input().split()))
+# 	target = int(input())
+# 	dp = [0] * (target + 1)
+# 	dp[0] = 1
+# 	for token in tokens:
+# 		tmp = [0] * (target + 1)
+# 		for i in range(target):
+# 			cnt = 1
+# 			while i + token * cnt <= target:
+# 				tmp[i + token * cnt] += dp[i]
+# 				cnt += 1
+# 		for i in range(target + 1):
+# 			dp[i] += tmp[i]
+# 	print(dp[-1])
+
 
 import sys
 input = sys.stdin.readline
 
-result = []
 t = int(input())
 for _ in range(t):
 	n = int(input())
 	tokens = list(map(int, input().split()))
 	target = int(input())
 	dp = [0] * (target + 1)
+	dp[0] = 1
 	for token in tokens:
-		i = 1
-		while i * token <  target:
-			dp[i * token] += 1
-			i += 1
-	tmp = 0
-	for i in range(0, target // 2 + 1):
-		# if dp[i] * dp[target - i] != 0:
-			# tmp += 1
-		tmp += (dp[i] * dp[target - i])
-	result.append(tmp)
+		print(dp)
+		for i in range(1, target + 1):
+			if i - token >= 0:
+				dp[i] += dp[i - token]
 	print(dp)
-for r in result:
-	print(r)
+	print(dp[-1])
