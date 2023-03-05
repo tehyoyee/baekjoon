60b62d630b0382282ae97f5f675051b23e764ff3
root <root@tehyoyee.localdomain>
2023-03-05 18:14:52 +0900

    add 12100.py

--- 12100.py
+++ 12100.py
@@ -22,21 +22,22 @@ def toRight(graph, cnt, n):
 							graph[i][tmp] = 0
 						tmp = -1
 					else:
+						graph[i][nj] = graph[i][tmp]
 						nj -= 1
 						if nj >= 0:
 							graph[i][nj] = graph[i][j]
-						tmp = j
+							if nj != j:
+								graph[i][j] = 0
+						tmp = -1
 					nj -= 1
 				else:
 					tmp = j
 		if nj != tmp and tmp != -1 and nj >= 0:
 			graph[i][nj], graph[i][tmp] = graph[i][tmp], 0
-	for x in graph:
-		print(x)
-	print()
-	result = max(result, toLower(graph, cnt + 1, n))
-	# result = max(result, toLeft(graph, cnt + 1, n))
-	# result = max(result, toUpper(graph, cnt + 1, n))
+	result = max(result, toRight(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toLeft(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toUpper(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toLower(copy.deepcopy(graph), cnt + 1, n))
 	return result
 
 def toLeft(graph, cnt, n):
@@ -58,21 +59,22 @@ def toLeft(graph, cnt, n):
 							graph[i][tmp] = 0
 						tmp = -1
 					else:
+						graph[i][nj] = graph[i][tmp]
 						nj += 1
 						if nj < n:
 							graph[i][nj] = graph[i][j]
-						tmp = j
+							if nj != j:
+								graph[i][j] = 0
+						tmp = -1
 					nj += 1
 				else:
 					tmp = j
 		if nj != tmp and tmp != -1 and nj < n:
 			graph[i][nj], graph[i][tmp] = graph[i][tmp], 0
-	for x in graph:
-		print(x)
-	print()
-	# result = max(result, toLower(graph, cnt + 1, n))
-	# result = max(result, toUpper(graph, cnt + 1, n))
-	# result = max(result, toRight(graph, cnt + 1, n))
+	result = max(result, toRight(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toLeft(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toUpper(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toLower(copy.deepcopy(graph), cnt + 1, n))
 	return result
 
 def toUpper(graph, cnt, n):
@@ -94,21 +96,22 @@ def toUpper(graph, cnt, n):
 							graph[tmp][j] = 0
 						tmp = -1
 					else:
+						graph[ni][j] = graph[tmp][j]
 						ni += 1
 						if ni < n:
 							graph[ni][j] = graph[i][j]
-						tmp = i
+							if ni != i:
+								graph[i][j] = 0
+						tmp = -1
 					ni += 1
 				else:
 					tmp = i
 		if ni != tmp and tmp != -1 and ni < n:
 			graph[ni][j], graph[tmp][j] = graph[tmp][j], 0
-	for x in graph:
-		print(x)
-	print()
-	# result = max(result, toLower(graph, cnt + 1, n))
-	# result = max(result, toLeft(graph, cnt + 1, n))
-	result = max(result, toRight(graph, cnt + 1, n))
+	result = max(result, toRight(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toLeft(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toUpper(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toLower(copy.deepcopy(graph), cnt + 1, n))
 	return result
 
 def toLower(graph, cnt, n):
@@ -130,32 +133,62 @@ def toLower(graph, cnt, n):
 							graph[tmp][j] = 0
 						tmp = -1
 					else:
+						graph[ni][j] = graph[tmp][j]
 						ni -= 1
 						if ni >= 0:
 							graph[ni][j] = graph[i][j]
-						tmp = i
+							if ni != i:
+								graph[i][j] = 0
+						tmp = -1
 					ni -= 1
 				else:
 					tmp = i
 		if ni != tmp and tmp != -1 and ni >= 0:
 			graph[ni][j], graph[tmp][j] = graph[tmp][j], 0
-	for x in graph:
-		print(x)
-	print()
-	result = max(result, toLeft(graph, cnt + 1, n))
-	# result = max(result, toUpper(graph, cnt + 1, n))
-	# result = max(result, toRight(graph, cnt + 1, n))
+	result = max(result, toRight(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toLeft(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toUpper(copy.deepcopy(graph), cnt + 1, n))
+	result = max(result, toLower(copy.deepcopy(graph), cnt + 1, n))
 	return result
 
 n = int(input())
 graph = []
 for _ in range(n):
 	graph.append(list(map(int, input().split())))
+result = []
+result.append(toRight(copy.deepcopy(graph), 0, n))
+result.append(toLeft(copy.deepcopy(graph), 0, n))
+result.append(toUpper(copy.deepcopy(graph), 0, n))
+result.append(toLower(copy.deepcopy(graph), 0, n))
+print(max(result))
 
-# print(toRight(copy.deepcopy(graph), 0, n))
-print(toLeft(copy.deepcopy(graph), 0, n))
-# print(toUpper(copy.deepcopy(graph), 0, n))
-# print(toLower(copy.deepcopy(graph), 0, n))
 
-# for x in graph:
-	# print(x)
+# 0 4 0
+# 2 0 2
+# 0 4 0
+
+
+# 3
+# 256 8 128
+# 16 0 256
+# 0 8 0
+# u
+# 256 16 128
+# 16 0 256
+# 0 0 0
+# r
+# 256 16 128
+# 0 16 256
+# 0 0 0
+# u
+# 256 32 128
+# 0 0 256
+# 0 0 0
+# l
+# 256 32 128
+# 256 0 0
+# 0 0 0
+# u
+# 512 32 128
+# 0 0 0
+# 0 0 0
