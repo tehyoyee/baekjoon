794d25ee25eef0c43dc62ce486d18c6e4b575868
taehykim <tehyoyee@gmail.com>
2023-03-29 11:18:42 +0900

    add 1238.py

--- 1238.py
+++ 1238.py
@@ -1,51 +1,34 @@
 # 1238.py
-# 23.03.09. 17:02 ~ 17:41
-
-# import sys
-# input = sys.stdin.readline
-
-# def dfs(i, x, cost, n):
-# 	result = 1000000000
-# 	if i == x:
-# 		return cost
-# 	for ni in range(1, n + 1):
-# 		if graph[i][ni] and not visit[ni]:
-# 			visit[ni] = 1
-# 			result = min(dfs(ni, x, cost + graph[i][ni], n), result)
-# 			visit[ni] = 0
-# 	return result
-
-# N, M, X = map(int, input().split())
-# result = [0] * (N + 1)
-# graph = [[0] * (N + 1) for _ in range(N + 1)]
-# visit = [0] * (N + 1)
-# for _ in range(M):
-# 	a, b, c = map(int, input().split())
-# 	graph[a][b] = c
-# for start in range(1, N + 1):
-# 	result[start] += (dfs(start, X, 0, N))
-# for start in range(1, N + 1):
-# 	result[start] += (dfs(X, start, 0, N))
-# print(max(result))
+# 23.03.29. 다익스트라 답지.
 
 import sys
 input = sys.stdin.readline
+from collections import deque
 
+result = 0
 N, M, X = map(int, input().split())
-result = [0] * (N + 1)
-graph = [[float("inf")] * (N + 1) for _ in range(N + 1)]
+graph = [[] for _ in range(N + 1)]
 for _ in range(M):
-	a, b, c = map(int, input().split())
-	graph[a][b] = c
-for x in graph:
-	print(x)
-print()
-for k in range(1, N + 1):
-	for i in range(1, N + 1):
-		for j in range(1, N + 1):
-			graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+	a, b, t = map(int, input().split())
+	graph[a].append((b, t))
+
+def dijkstra(i, X):
+	q = deque([])
+	time = [10000000] * (N + 1)
+	time[i] = 0
+	q.append((0, i))
+	while q:
+		ct, ci = q.popleft()
+		if ct > time[ci]:
+			continue
+		for ni, t in graph[ci]:
+			nt = ct + t
+			if time[ni] > nt:
+				time[ni] = nt
+				q.append((nt, ni))
+	return time[X]
+
 for i in range(1, N + 1):
-	result[i] += (graph[i][X] + graph[X][i])
-for x in graph:
-	print(x)
-print(max(result))
+	result = max(result, (dijkstra(i, X) + dijkstra(X, i)))
+
+print(result)
--- 1753.py
+++ 1753.py
@@ -1,31 +1,34 @@
 # 1753.py
-
 import sys
-import math
+import heapq
 input = sys.stdin.readline
+INF = sys.maxsize
+V, E = map(int, input().split())
+K = int(input())
+dp = [INF]*(V+1)
+heap = []
+graph = [[] for _ in range(V + 1)]
+def Dijkstra(start):
+	dp[start] = 0
+	heapq.heappush(heap,(0, start))
+	while heap:
+		print(heap)
+		wei, now = heapq.heappop(heap)
+		if dp[now] < wei:
+			continue
+		for w, next_node in graph[now]:
+			next_wei = w + wei
+			if next_wei < dp[next_node]:
+				dp[next_node] = next_wei
+				heapq.heappush(heap,(next_wei,next_node))
+for _ in range(E):
+	u, v, w = map(int, input().split())
+	graph[u].append((w, v))
 
-v, e = map(int, input().split())
-s = int(input())
-graph = [[] for _ in range(v + 1)]
-w = [math.inf for _ in range(v + 1)]
-for _ in range(e):
-	a, b, c = map(int, input().split())
-	graph[a].append([b, c])
 
-w[s] = 0
-q = [[s, 0]]
-while q:
-	cur, cur_w = q.pop(0)
-	if w[cur] < cur_w:
-		continue
-	for x in graph[cur]:
-		if w[x[0]] > cur_w + x[1]:
-			q.append([x[0], cur_w + x[1]])
-
-for x in range(1, v + 1):
-	print("INF") if w[x] == math.inf else print(w[x])
-
-
+Dijkstra(K)
+for i in range(1,V+1):
+	print("INF" if dp[i] == INF else dp[i])
 
 
 # import sys
