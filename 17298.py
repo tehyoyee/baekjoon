88cb26d276b1a326c25ac29f5dea0461cbc1385b
taehykim <tehyoyee@gmail.com>
2023-03-29 09:44:55 +0900

    add 17298.py

---
+++ 17298.py
@@ -0,0 +1,17 @@
+# 17298.py
+# 23.03.29. 09:27 ~ 09:41
+
+N = int(input())
+A = list(map(int, input().split()))
+NGE = [-1] * N
+stack = []
+
+for i in range(0, N):
+	if len(stack):
+		while stack[-1][0] < A[i]:
+			NGE[stack.pop()[1]] = A[i]
+			if not len(stack):
+				break
+	stack.append([A[i], i])
+	
+print(*NGE)
