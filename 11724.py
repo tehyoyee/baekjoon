121c705082aac499a30f7b3c7ff29f3fc0490d2a
root <root@tehyoyee.localdomain>
2023-04-27 08:51:54 +0900

    add 1937.py

--- 11724.py
+++ 11724.py
@@ -7,25 +7,45 @@ input = sys.stdin.readline
 def bfs():
 	result = 0
 	for x in range(1, n + 1):
-		if visited[x]:
+		if visited{x}:
 			continue
 		result += 1
-		queue = deque([x])
-		visited[x] = True
+		queue = deque({x})
+		visited{x} = True
 		while queue:
 			x = queue.popleft()
-			for i in graph[x]:
-				if not visited[i]:
+			for i in graph{x}:
+				if not visited{i}:
 					queue.append(i)
-					visited[i] = True
+					visited{i} = True
 	print(str(result))
 
 n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
+graph = {{} for _ in range(n + 1)}
 for _ in range(m):
 	a, b = map(int, input().split())
-	graph[a].append(b)
-	graph[b].append(a)
-visited = [False] * (n + 1)
+	graph{a}.append(b)
+	graph{b}.append(a)
+visited = {False} * (n + 1)
 
 bfs()
+
+ public static void main(String{} args) {
+        String{} kor = {"AAA", "BCD", "AAAAA", "ZY"};
+        String{} usa = {"AB", "AA", "TTTT"};
+        String{} incs = {"AB BCD AA AAA TTTT AAAAA", "BCD AAA", "AB AAA TTTT BCD", "AA AAAAA AB", "AAA AB BCD"};
+        int answer = solution(kor, usa, incs);
+        System.out.println(answer); // prints 2
+    }
+
+
+Stringfolders = {{"animal", "root"}, {"fruit", "root"}}
+files = {{"cat", "1B", "animal"}, {"dog", "2B", "animal"}, {"apple", "4B", "fruit"}}
+selected = {"animal"}
+excepted = {"apple"}
+print(solution(folders, files, selected, excepted))
+
+folders = {{"food", "root"}, {"meat", "food"}, {"fruit", "food"}, {"animal", "root"}}
+files = {{"cat", "1B", "animal"}, {"dog", "2B", "animal"}, {"fork", "1KB", "meat"}, {"beef", "8KB", "meat"}, {"apple", "4B", "fruit"}, {"fire", "83B", "root"}}
+selected = {"root", "meat"}
+excepted = {"fork", "apple"}
--- asdf.py
+++ asdf.py
@@ -1,4 +1,27 @@
+from collections import defaultdict
 
+def solution(kor, usa, incs):
+    answer = 0
 
-for i in range(20):
-	print(i**2)
+    country = {}
+    for i in kor:
+        country[i] = 0
+    for i in usa:
+        country[i] = 1
+    combs = defaultdict(int)
+    for stocks in incs:
+        stocks = stocks.split()
+
+        for i in range(len(stocks)):
+            for j in range(i+1, len(stocks)):
+                if country[stocks[i]] != country[stocks[j]]:
+                    if country[stocks[i]] == 0:
+                        combs[(stocks[i], stocks[j])] += 1
+                    else:
+                        combs[(stocks[j], stocks[i])] += 1
+
+    if len(combs) == 0:
+        return 0
+    combs = sorted(combs.values(), reverse=True)
+
+    return(combs[0])
