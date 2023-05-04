67d60937a14de5e0239467be192d72a7d86eec1a
root <root@tehyoyee.localdomain>
2023-05-04 11:24:56 +0900

    Add 1300.py

---
+++ 1300.py
@@ -0,0 +1,20 @@
+# 1300.py
+# 23.05.04. 09:46 ~
+
+N = int(input())
+k = int(input())
+
+start = 1
+end = N**2
+while start <= end:
+	mid = (end + start) // 2
+	cnt = 0
+	for i in range(1, N+1):
+		cnt += min(mid//i, N)
+	if cnt >= k:
+		end = mid-1
+	else:
+		start = mid+1
+print(start)
+
+# 1 2 2 3 3 4 6 6 9
