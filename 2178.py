5aa3977026c95bcfdabf80667ed1137f3002e8d4
root <root@tehyoyee.localdomain>
2023-05-12 21:24:18 +0900

    add 2178.py

--- 2178.py
+++ 2178.py
@@ -1,36 +1,9 @@
 # 2178.py
-# 23.05.12. 21:16 ~
-
-# import sys
-# sys.setrecursionlimit(10**5)
-# input = sys.stdin.readline
-
-# di = [0, 0, 1, -1]
-# dj = [1, -1, 0, 0]
-
-# def dfs(ci, cj):
-# 	if ci == N - 1 and cj == M - 1:
-# 		return visit[ci][cj]
-# 	for k in range(4):
-# 		ni, nj = ci + di[k], cj + dj[k]
-# 		if 0 <= ni < N and 0 <= nj < M and graph[ni][nj] == 1 and visit[ni][nj] > visit[ci][cj]:
-# 			visit[ni][nj] = visit[ci][cj] + 1
-# 			dfs(ni, nj)
-
-
-# N, M = map(int, input().split())
-# graph = []
-# visit = [[30000] * M for _ in range(N)]
-# for _ in range(N):
-# 	graph.append(list(map(int, input().rstrip())))
-# visit[0][0] = 1
-# dfs(0, 0)
-# print(visit[N-1][M-1])
+# 23.05.12.
 
 import sys
 from collections import deque
 input = sys.stdin.readline
-
 
 di = [0, 0, 1, -1]
 dj = [1, -1, 0, 0]
@@ -52,4 +25,4 @@ while q:
 		if 0 <= ni < N and 0 <= nj < M and graph[ni][nj] and not visit[ni][nj]:
 			visit[ni][nj] = steps + 1
 			q.append([ni, nj])
-print(steps)	
+print(steps)
