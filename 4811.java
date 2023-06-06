7f4fccbfe1c14c291e5a8654756a23571d9d53d7
root <root@tehyoyee.localdomain>
2023-06-07 08:56:25 +0900

    add 4811.java

--- 4811.java
+++ 4811.java
@@ -10,6 +10,7 @@ class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		dp = new Long[31];
 
 		dp[0] = 1L;
