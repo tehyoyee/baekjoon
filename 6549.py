372732d06b6acc51af4b688324a2e215227c1c19
taehykim <tehyoyee@gmail.com>
2023-07-09 23:34:47 +0900

    add 6549.py

--- 6549.py
+++ 6549.py
@@ -1,13 +1,26 @@
 # 6549.py
-# 23.05.04. 11:26 ~
+# 23.05.04. 11:26 ~ x
 
 import sys
 input = sys.stdin.readline
 
+result = []
 while True:
-	lst = list(map(int, input().split()))
-	if len(lst) == 1 and lst[0] == 0:
+	N, *lst = map(int, input().split())
+	if N == 0:
 		break
+		
+	maxSize = 0
 	stack = []
-	graph = []
-	
+
+	for i in range(N):
+		min_i = i
+		while stack and stack[-1][0] >= lst[i]:
+			h, min_i = stack.pop()
+			maxSize = max(maxSize, h * (i-min_i))
+		stack.append((lst[i], min_i))
+
+	for h, i in stack:
+		maxSize = max(maxSize, (N-i)*h)
+
+	print(maxSize)
