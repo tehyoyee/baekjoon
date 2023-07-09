38b4a7086c0ae4e565c5c5bd0ca60126861158ac
taehykim <tehyoyee@gmail.com>
2023-07-09 18:10:23 +0900

    Add 1182.py

--- 1182.py
+++ 1182.py
@@ -1,18 +1,24 @@
 # 1182.py
-# 23.07.09. 17:33 ~
+# 23.07.09. 17:33 ~ 18:10
 
 import sys
 input = sys.stdin.readline
 
+global result
+
+def dfs(v, idx, N, S):
+	global result
+
+	if v == S:
+		result += 1
+	for i in range(idx + 1, N):
+		dfs(v + lst[i], i, N, S)
+
 result = 0
 N, S = map(int, input().split())
-lst = list(map(int, input().split()))
-acc = [0] * (N + 1)
-for i in range(N):
-	acc[i + 1] = acc[i] + lst[i]
+lst = list(map(int, input().split())) + [0]
+visit = [False] * (N + 1)
 
 for i in range(N):
-	for j in range(i + 1, N + 1):
-		if acc[j] - acc[i] == S:
-			result += 1
+	dfs(lst[i], i, N, S)
 print(result)
