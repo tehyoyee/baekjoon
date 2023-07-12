fc7aa2166491ee9f279ddfc6dd6b05034992f7b6
root <root@tehyoyee.localdomain>
2023-07-13 04:49:59 +0900

    Add 25083.java

---
+++ 25083.java
@@ -0,0 +1,15 @@
+// 25083.java
+// 23.07.13. 04:43
+
+class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("         ,r\'\"7");
+		System.out.println("r`-_   ,'  ,/");
+ 		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
