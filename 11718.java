60283a3e096b6a95c6004f115352b1139ea78f3f
root <root@tehyoyee.localdomain>
2023-07-13 04:04:22 +0900

    add 11718.java

---
+++ 11718.java
@@ -0,0 +1,17 @@
+// 11718.java
+// 23.07.13. 03:59
+
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNext()) {
+			System.out.println(sc.nextLine());
+		}
+		sc.close();
+	}
+}
