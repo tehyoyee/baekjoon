4cf4c77b13d60e56c623dd1f312f0d5aac5ecf90
root <root@tehyoyee.localdomain>
2023-07-13 02:31:09 +0900

    add 18108.java

---
+++ 18108.java
@@ -0,0 +1,14 @@
+// 18108.java
+// 23.07.13. 02:29
+
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()-543);
+		sc.close();
+	}
+}
