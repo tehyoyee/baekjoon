9deb0ae2c437712a2bb0c76b100475372de390ce
root <root@tehyoyee.localdomain>
2023-07-15 01:02:40 +0900

    add 2743.java

---
+++ 2743.java
@@ -0,0 +1,15 @@
+// 2743.java
+// 23.07.15. 01:00
+
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println(sc.nextLine().length());
+		sc.close();
+	}
+}
