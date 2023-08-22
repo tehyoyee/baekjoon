49917e6a32269bf7eb61147531d92ae013963e5b
Taehyeong Kim <89984263+tehyoyee@users.noreply.github.com>
2023-08-22 16:50:39 +0900

    [Silver III] Title: 두 수의 합, Time: 320 ms, Memory: 25504 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,37 @@
+// 30
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int result = 0;
+		int n = Integer.parseInt(br.readLine());
+		int[] lst = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			lst[i] = Integer.parseInt(st.nextToken());
+		}
+		int target = Integer.parseInt(br.readLine());
+		int s = 0;
+		int e = n - 1;
+		Arrays.sort(lst);
+		while (s < e) {
+			int x = lst[s] + lst[e];
+			if (target == x) {
+				s++;
+				e--;
+				result++;
+			} else if (target > x) {
+				s++;
+			} else {
+				e--;
+			}
+		}
+		System.out.println(result);
+	}
+}
