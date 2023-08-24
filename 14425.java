37edd872bba0a9cdd989fccdf90951ab01f667f3
root <root@tehyoyee.localdomain>
2023-08-24 15:15:20 +0900

    add 14425.java

---
+++ 14425.java
@@ -0,0 +1,29 @@
+// 14425.java
+// 23.08.24. 15:06 ~ 15:12
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int result = 0;
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		HashMap<String, Boolean> map = new HashMap<>();
+
+		for (int i = 0; i < N; i++) {
+			map.put(br.readLine(), true);
+		}
+		for (int i = 0; i < M; i++) {
+			if (map.containsKey(br.readLine())) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+}
