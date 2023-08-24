5c2070c15fe4ec816277ca798e1a21304f24343b
root <root@tehyoyee.localdomain>
2023-08-24 15:02:39 +0900

    add 2018.java

---
+++ 2018.java
@@ -0,0 +1,32 @@
+// 2018.java
+// 23.08.24. 13:23 ~
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N + 1];
+		for (int i = 1; i < N + 1; i++) {
+			arr[i] = i + arr[i-1];
+		}
+		int result = 0;
+		int s = 0;
+		int e = 0;
+		while (e <= N) {
+			if (arr[e] - arr[s] == N) {
+				s++;
+				result++;
+			} else if (arr[e] - arr[s] < N) {
+				e++;
+			} else {
+				s++;
+			}
+		}
+		System.out.println(result);
+
+	}
+}
