a3fef1ddc64b158125281d7577ac84532395861f
Taehyeong Kim <89984263+tehyoyee@users.noreply.github.com>
2023-08-30 15:33:52 +0900

    [Silver V] Title: 수들의 합, Time: 132 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,21 @@
+// 18
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		long N = Long.parseLong(br.readLine());
+		long sum = 0;
+		long i = 2;
+		while (true) {
+			sum += i;
+			if (sum >= N) { break; }
+			i++;
+		}
+		System.out.println(--i);
+	}
+}
