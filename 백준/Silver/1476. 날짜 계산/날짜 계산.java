c3a8e05d8ca35d7b49361afcf20409d1be83d407
Taehyeong Kim <89984263+tehyoyee@users.noreply.github.com>
2023-10-18 11:47:22 +0900

    [Silver V] Title: 날짜 계산, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -0,0 +1,39 @@
+//18
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int E = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int result = 1;
+
+		while (true) {
+			if (E == S && S == M)
+				break;
+			int x = function(E, S, M);
+			if (x == 0) {
+				E += 15;
+			} else if (x == 1) {
+				S += 28;
+			} else {
+				M += 19;
+			}
+		}
+		System.out.println(E);
+	}
+
+	public static int function(int E, int S, int M) {
+		if (E <= S && E <= M)
+			return 0;
+		if (M < E && M <= S)
+			return 2;
+		return 1;
+	}
+}
