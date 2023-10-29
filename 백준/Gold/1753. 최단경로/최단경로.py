ef21bdfbd6e6ca6078859a3875139e8ff5e71e31
Taehyeong Kim <89984263+tehyoyee@users.noreply.github.com>
2023-10-29 23:47:07 +0900

    [Gold IV] Title: 최단경로, Time: 672 ms, Memory: 68516 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,34 @@
+# 10분
+
+import sys
+input = sys.stdin.readline
+import heapq
+
+V, E = map(int, input().split())
+K = int(input())
+graph = [[] for _ in range(V + 1)]
+for _ in range(E):
+	u, v, w = map(int, input().split())
+	graph[u].append((v, w))
+
+# def dijkstra(i):
+dp = [10000000] * (V + 1)
+dp[K] = 0
+
+q = []
+heapq.heappush(q, (0, K))
+while q:
+	cur_cost, cur_i = heapq.heappop(q)
+	if cur_cost > dp[cur_i]:
+		continue
+	for new_i, next_cost in graph[cur_i]:
+		new_cost = next_cost + cur_cost
+		if new_cost < dp[new_i]:
+			dp[new_i] = new_cost
+			heapq.heappush(q, (new_cost, new_i))
+
+for x in range(1, V + 1):
+	if dp[x] == 10000000:
+		print("INF")
+	else:
+		print(dp[x])
