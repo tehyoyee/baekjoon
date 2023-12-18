f4c62bd0d1f92f63f67b3e13ea78a92fa0613207
Taehyeong Kim <89984263+tehyoyee@users.noreply.github.com>
2023-12-18 00:31:06 -0800

    [Silver II] Title: 최대 힙, Time: 332 ms, Memory: 26416 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,29 @@
+// 6분
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int input = Integer.parseInt(br.readLine());
+
+			if (input == 0) {
+				if (q.isEmpty()) {
+					sb.append("0\n");
+				} else {
+					sb.append(-q.poll()).append("\n");
+				}
+			} else {
+				q.add(-input);
+			}
+		}
+		System.out.print(sb);
+	}
+}
