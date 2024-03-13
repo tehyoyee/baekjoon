ee1c578eaaaa0cab6b6fe1e3a1c8fa34dd0784cc
taehyeong <tehyoyee@gmail.com>
2024-03-13 11:32:17 +0900

    add 5639.py

--- 5639.py
+++ 5639.py
@@ -1,13 +1,28 @@
 # 5639.py
-# 23.04.26. 11:30 ~
+# 24.03.13. 10:51 ~ 11:30
 
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-graph = []
+def runPostOrder(rootIdx, endIdx):
+	if rootIdx > endIdx:
+		return
+	rightRootIdx = endIdx + 1
+	for i in range(rootIdx + 1, endIdx + 1):
+		if lst[rootIdx] < lst[i]:
+			rightRootIdx = i
+			break
+
+	runPostOrder(rootIdx + 1, rightRootIdx - 1)
+	runPostOrder(rightRootIdx, endIdx)
+	print(lst[rootIdx])
+
+lst = []
 while True:
 	try:
-		graph.append(int(input()))
+		lst.append(int(input().rstrip()))
 	except:
 		break
-	
+
+runPostOrder(0, len(lst) - 1)
